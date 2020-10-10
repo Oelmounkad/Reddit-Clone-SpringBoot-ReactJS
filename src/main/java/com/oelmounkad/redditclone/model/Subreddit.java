23c102b019f48f8f8a031ff2541d72366fafec00
Elmounkad Oussama <oelmounkad@gmail.com>
2020-10-11 01:13:18 +0200

    added models

---
+++ src/main/java/com/oelmounkad/redditclone/model/Subreddit.java
@@ -0,0 +1,34 @@
+package com.oelmounkad.redditclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Subreddit {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotNull(message = "Community name is required")
+    private String name;
+    @NotNull(message = "Description is required")
+    private String description;
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    private User user;
+}
---
+++ src/main/java/com/oelmounkad/redditclone/model/User.java
@@ -0,0 +1,36 @@
+package com.oelmounkad.redditclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
+}
