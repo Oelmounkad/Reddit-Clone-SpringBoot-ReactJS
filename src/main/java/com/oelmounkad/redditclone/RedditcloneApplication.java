2f37d37fd454b91cfa0ff84554a91fb746edb942
Elmounkad Oussama <oelmounkad@gmail.com>
2020-10-10 18:32:20 +0200

    first commit

---
+++ src/main/java/com/oelmounkad/redditclone/RedditcloneApplication.java
@@ -0,0 +1,13 @@
+package com.oelmounkad.redditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditcloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oelmounkad/redditclone/RedditcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oelmounkad.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
